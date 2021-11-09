9dbed80d3017fb385bb6264784758bc303c4e4e6
GermanATA <ga.torres2283@gmail.com>
2021-11-09 02:40:49 -0500

    "subir a github"

---
+++ app/src/main/java/german/appprimeraclase/ActividadPrincipal.kt
@@ -0,0 +1,11 @@
+package german.appprimeraclase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_principal)
+    }
+}
